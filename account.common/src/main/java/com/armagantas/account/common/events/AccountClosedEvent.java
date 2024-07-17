7c9bad51bb162e1a2cdabd42e1b090eeca5551fd
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2024-07-17 15:04:48 +0300

    common operations are done.

---
+++ account.common/src/main/java/com/armagantas/account/common/events/AccountClosedEvent.java
@@ -0,0 +1,13 @@
+package com.armagantas.account.common.events;
+
+import com.armagantas.cqrs.core.events.BaseEvent;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+public class AccountClosedEvent extends BaseEvent {
+
+}
